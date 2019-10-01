f464f7a089bff07d05b3927a2727fc0f8cb15a1a
ruby <ruby@localhost>
2019-09-30 22:02:50 -0700

    yeehaw hope this works cri

---
+++ lab1a/src/Lab1a.java
@@ -0,0 +1,13 @@
+
+public class Lab1a {
+
+	public static void main(String[] args) {
+		String x = "Sukhjit Singh Bob";
+		System.out.printf("My name is %-27s\t%10d\t%-10.2f",x,55,1054.45123f);
+		System.out.printf("This is a test.");
+
+
+
+	}
+
+}
