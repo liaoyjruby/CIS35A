ed0093e7c2e22385fe44c56e37ced4450b17c35d
ruby <ruby@localhost>
2019-10-15 14:11:43 -0700

    Looped de dooped the driver yeehaw

--- src/Driver.java
+++ src/Driver.java
@@ -6,31 +6,36 @@ public class Driver {
 
 	public static void main(String[] args) {
 
-		System.out.printf("Please input personal data.");
+		// Store each entry in Person[] array; maximum of 10 entries
+		Person[] personArray = new Person[10];
 
-		// Person 1
-		Person a1 = new Person();
-		readData(a1);
-		a1.print();
+		// Loop to ask if more entries needed
+		System.out.printf("Please input each person's information.");
+		int count = 0;
+		String next;
+		do {
+			next = "n";
+			personArray[count] = new Person();
+			input(personArray[count]);
+			System.out.printf("\nPerson %d", count + 1);
+			personArray[count].print();
+			count++;
 
-		System.out.printf("\n\nNext entry:");
+			System.out.printf("\nAdd another entry? (Y/N)  > ");
+			next = in.next();
+		} while (count < 10 && next.substring(0, 1).equalsIgnoreCase("y"));
 
-		// Person 2
-		Person a2 = new Person();
-		readData(a2);
-		a2.print();
-
-		System.out.printf("\n\nNext entry:");
-
-		// Person 3
-		Person a3 = new Person();
-		readData(a3);
-		a3.print();
+		// Ask if user wants to reprint previous inputs
+		System.out.printf("Input complete. Display all entries? (Y/N)  > ");
+		next = in.next();
+		if (next.substring(0, 1).equalsIgnoreCase("y")) {
+			printAll(personArray, count);
+		}
 
 	}
 
 	// Prompt for and store data inputted through Scanner
-	public static void readData(Person a) {
+	public static void input(Person a) {
 		// Prompt for and store name as string
 		System.out.printf("\n\nEnter full name: ");
 		a.setName(in.next());
@@ -49,14 +54,10 @@ public class Driver {
 		a.setAge(in.nextInt());
 		a.setAge(checkNeg(a.getAge()));
 
-		// Prompt for and store phone number as int
+		// Prompt for and store phone number as string
 		System.out.printf("\nEnter 10-digit phone number (ex.4081234567): ");
-		while (!in.hasNextInt()) {
-			System.out.printf("\nEnter valid number (no punctuation): ");
-			in.next();
-		}
-		a.setPhone(in.nextInt());
-		a.setPhone(checkNeg(a.getPhone()));
+		a.setPhone(in.next());
+		a.setPhone(checkLength(a.getPhone()));
 
 	}
 
@@ -69,5 +70,26 @@ public class Driver {
 		}
 		result = input;
 		return result;
+	}
+
+	// Ensure 10 digit phone number input
+	public static String checkLength(String input) {
+		String result;
+		while (input.length() < 10 || input.length() > 10) {
+			System.out.printf("\nPlease enter a 10 digit phone number (ex.4081234567):");
+			input = in.next();
+		}
+		result = input;
+		return result;
+	}
+
+	// Display all entries
+	public static void printAll(Person[] personArray, int entries) {
+		int count;
+		for (count = 0; count < entries; count++) {
+			System.out.printf("\nPerson %d", count + 1);
+			personArray[count].print();
+		}
+		System.out.printf("\nTotal entries: %d", entries);
 	}
 }
--- src/Person.java
+++ src/Person.java
@@ -1,24 +1,20 @@
 public class Person {
 
-	private String name;
-	private String address;
+	private String name, address, phone;
 	private int age;
-	private int phone;
-
-	private static int numberOfPeople = 0;
-	// counter
 
 	public Person() {
-		numberOfPeople++;
 	}
 
-	public Person(String name, String address, int age, int phone) {
+	// Default constructor
+	public Person(String name, String address, int age, String phone) {
 		this.name = name;
 		this.address = address;
 		this.age = age;
 		this.phone = phone;
 	}
 
+	// Getters and setters
 	public String getName() {
 		return name;
 	}
@@ -43,17 +39,38 @@ public class Person {
 		this.age = age;
 	}
 
-	public int getPhone() {
+	public String getPhone() {
 		return phone;
 	}
 
-	public void setPhone(int phone) {
+	public void setPhone(String phone) {
 		this.phone = phone;
 	}
 
+	// Ensures capitalization of each word
+	public String formatName(String name) {
+		String nameFormat = name.substring(0, 1).toUpperCase() + name.substring(1);
+
+		for (int i = 0; i < nameFormat.length(); i++) {
+			if (nameFormat.substring(i, i + 1).equals(" ")) {
+				nameFormat = nameFormat.substring(0, i + 1) + nameFormat.substring(i + 1, i + 2).toUpperCase()
+						+ nameFormat.substring(i + 2);
+			}
+		}
+		return nameFormat;
+	}
+
+	// Formats phone number to readable form
+	public String formatPhone(String phone) {
+		String phoneFormat = "phone";
+		phoneFormat = "(" + phone.substring(0, 3) + ")" + phone.substring(3, 6) + "-" + phone.substring(6);
+		return phoneFormat;
+	}
+
+	// Prints all inputs for objects
 	public void print() {
-		System.out.printf("\nPerson %d\nName: %s\nAddress: %s\nAge: %d\nPhone number: %s\n", numberOfPeople, name,
-				address, age, phone);
+		System.out.printf("\nName: %s\nAddress: %s\nAge: %d\nPhone number: %s\n",formatName(name), formatName(address),
+				age, formatPhone(phone));
 	}
 
 }
