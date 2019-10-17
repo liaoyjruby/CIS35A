16bd2b9cdb8123aa91950d67c4ed56590fe1f76f
ruby <ruby@localhost>
2019-10-16 18:35:01 -0700

    comments from lecture

--- src/Coin.java
+++ src/Coin.java
@@ -1,4 +1,8 @@
 
 public class Coin {
-
+// constructor
+	//use random class to toss coin with heads or tail
+	// write toss method that can be called before you toss the coin once
+	// toss will have random generator to decide if heads or tail
+	// can call toss method in CONSTRUCTOR so when coin instanced it will have either heads or tails
 }
--- src/Simulation.java
+++ src/Simulation.java
@@ -1,4 +1,15 @@
 
 public class Simulation {
+// tossForSimulation()
+	// pass coin to it and tell it how many times to toss it (ex. 10, 20)
+	// sets values of # of heads and tails automatically
+	
+	//print stats (Coin n ) --> can simply print values of heads and tails 
+	// can put print method in tossForSimulation
+		// or not so it's clearer
+	
+	// also do a class diagram
+	
+	// ..void tossforsimulation(Coin n, int count) //loop in which coin is tossed and stats tracked.
 
 }
