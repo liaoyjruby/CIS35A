29781f7de4c98e3bd6dd5edf0d0712116551d852
Rubsy <ruby516@gmail.com>
2019-10-21 01:35:26 -0700

    added assignment files

---
+++ src/Driver.java
@@ -0,0 +1,10 @@
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FileIO a1 = new FileIO("C:\\Users\\rubsy\\git\\Lab3\\src\\Salesdat.txt");
+		Franchise f = a1.readData();
+	}
+
+}
