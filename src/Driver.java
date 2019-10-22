6f9f858359c65ac4eed65d36f52a899e71a06d90
ruby <ruby@localhost>
2019-10-21 19:23:11 -0700

    lame

--- src/Driver.java
+++ src/Driver.java
@@ -2,9 +2,11 @@
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		FileIO a1 = new FileIO("C:\\Users\\rubsy\\git\\Lab3\\src\\Salesdat.txt");
-		Franchise f = a1.readData();
+		// Windows: C:\\Users\\rubsy\\git\\Lab3\\src\\Salesdat.txt
+		// Linux: /home/ruby/git/Lab3/src/Salesdat.txt
+		FileIO a1 = new FileIO("/home/ruby/git/Lab3/src/Salesdat.txt"); // Put appropriate fully-qualified file path
+		Franchise f = a1.readData(); // returns franchise information
+		System.out.println("Reading data done!");
 	}
 
 }
